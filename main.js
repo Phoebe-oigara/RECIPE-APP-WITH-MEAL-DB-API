b5da00242a05d71ba0db0cb0a66aa33057a59345
Phoebe Oigara <phoebeoigara@gmail.com>
2023-04-08 22:35:56 +0300

    functional fetch for various meals based on search.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const searchButton = getElementbyID('search-button');
+const searchButton = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
@@ -6,4 +6,37 @@ const recipeCloseBtn = document.getElementById('recipe-close-btn');
 // Adding event listeners to the page
 
 searchButton.addEventListener('click', getMealList);
-mealList.addEventListener('click', getRecipe);
+//mealList.addEventListener('click', getRecipe);
+
+//fetching the mealList from the MealDb API
+function getMealList(){
+    let searchText = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchText}`)
+    .then(response => response.json())
+    .then(fetchMeal)
+}
+
+//function to fetch the meals
+function fetchMeal (data) {
+let html = "";
+if(data.meals){
+    data.meals.forEach(meal => {
+        html += `
+        <div class="meal-item" data-id="${meal.idMeal}">
+        <div class ="meal-img">
+        <img src = "${meal.strMealThumb}" alt = "food picture">
+        </div>
+            <div class = "meal-name">
+             <h3>${meal.strMeal}</h3>
+                <a href = "#" class = "recipe-btn"> Get The Recipe</a>
+            </div>
+        </div>
+        `;
+    })
+    mealList.classList.remove("notFound");
+} else {
+    html = "Apologies, our recipes don't include that meal";
+    mealList.classList.add("notFound");
+    }
+    mealList.innerHTML = html;
+}
